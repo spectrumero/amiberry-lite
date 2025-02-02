3b1a1b5b5ee2d4fe061b467b2722b46d5900225f
Dimitris Panokostas <midwan@gmail.com>
2025-02-02 18:37:52 +0100

    Bump version to 5.8.3

--- src/main.cpp
+++ src/main.cpp
@@ -77,7 +77,7 @@
 #include "keyboard.h"
 
 // Special version string so that AmigaOS can detect it
-static constexpr char __ver[40] = "$VER: Amiberry-Lite 5.8.2 (2025-01-29)";
+static constexpr char __ver[40] = "$VER: Amiberry-Lite 5.8.3 (2025-02-02)";
 
 long int version = 256 * 65536L * UAEMAJOR + 65536L * UAEMINOR + UAESUBREV;
 
